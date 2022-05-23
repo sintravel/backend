f2be12b3e23b8f60e8201b1cf41a56e6442cf683
jihwan2da <88091743+jihwan2da@users.noreply.github.com>
2022-05-23 22:56:36 +0900

    feat: basetimeentity 구현

---
+++ src/main/java/backend/sintravel/domain/common/entity/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package backend.sintravel.domain.common.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime created_at;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updated_at;
+}
