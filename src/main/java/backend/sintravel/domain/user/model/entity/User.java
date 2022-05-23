7e80cdb121d1d41a02e2aa058de2e7ba3e409b7d
jihwan2da <88091743+jihwan2da@users.noreply.github.com>
2022-05-23 23:14:13 +0900

    chore: user entity 수정(setter -> getter)

--- src/main/java/backend/sintravel/domain/user/model/entity/User.java
+++ src/main/java/backend/sintravel/domain/user/model/entity/User.java
@@ -2,12 +2,13 @@ package backend.sintravel.domain.user.model.entity;
 
 import backend.sintravel.domain.common.entity.BaseTimeEntity;
 import backend.sintravel.domain.user.model.enums.UserType;
+import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
-@Setter
+@Getter
 public class User extends BaseTimeEntity {
 
     @Id
