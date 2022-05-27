1e897b88e3aa8e501f9c5929e3c25af06c51f779
jihwan2da <88091743+jihwan2da@users.noreply.github.com>
2022-05-27 23:24:35 +0900

    feat: User entity에 UserRole 추가

---
+++ src/main/java/backend/sintravel/domain/user/UserRole.java
@@ -0,0 +1,5 @@
+package backend.sintravel.domain.user;
+
+public enum UserRole {
+    ROLE_ADMIN, ROLE_USER
+}
--- src/main/java/backend/sintravel/domain/user/model/entity/User.java
+++ src/main/java/backend/sintravel/domain/user/model/entity/User.java
@@ -1,11 +1,13 @@
 package backend.sintravel.domain.user.model.entity;
 
 import backend.sintravel.domain.common.entity.BaseTimeEntity;
+import backend.sintravel.domain.user.UserRole;
 import backend.sintravel.domain.user.model.enums.UserType;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
@@ -16,15 +18,19 @@ public class User extends BaseTimeEntity {
     @Column(name = "user_id")
     private Long id;
 
-    @Column
-    private String email;
+    @Column(length = 80, unique = true)
+    private String username;
 
     private String password;
 
+    @Column(length = 80, unique = true)
     private String nickname;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "type")
     private UserType type;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private List<UserRole> roles;
 }
