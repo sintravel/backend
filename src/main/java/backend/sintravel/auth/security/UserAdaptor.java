88a14d85e9d864d4d94e1d703cd44d0cca1b3028
jihwan2da <88091743+jihwan2da@users.noreply.github.com>
2022-05-31 00:26:47 +0900

    feat: UserDetails 구현

---
+++ src/main/java/backend/sintravel/auth/security/UserAdaptor.java
@@ -0,0 +1,56 @@
+package backend.sintravel.auth.security;
+
+import backend.sintravel.domain.user.model.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class UserAdaptor implements UserDetails {
+
+    private final User user;
+
+    public UserAdaptor(User user) {
+        this.user = user;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(user.getAuthority().getRole()));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
