f07004acd2e5534d3a4a022f249571c0a66b6d6d
jihwan2da <88091743+jihwan2da@users.noreply.github.com>
2022-05-31 00:08:54 +0900

    chore: user entity 수정

--- src/main/java/backend/sintravel/domain/user/model/entity/User.java
+++ src/main/java/backend/sintravel/domain/user/model/entity/User.java
@@ -2,7 +2,10 @@ package backend.sintravel.domain.user.model.entity;
 
 import backend.sintravel.domain.common.entity.BaseTimeEntity;
 import backend.sintravel.domain.user.UserRole;
+import backend.sintravel.domain.user.model.enums.Authority;
 import backend.sintravel.domain.user.model.enums.UserType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,16 +14,17 @@ import java.util.List;
 
 @Entity
 @Getter
+@AllArgsConstructor
 public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
     private Long id;
 
     @Column(length = 80, unique = true)
     private String username;
 
+    @Column(name = "password")
     private String password;
 
     @Column(length = 80, unique = true)
@@ -30,7 +34,8 @@ public class User extends BaseTimeEntity {
     @Column(name = "type")
     private UserType type;
 
-    @ElementCollection(fetch = FetchType.EAGER)
     @Enumerated(EnumType.STRING)
-    private List<UserRole> roles;
+    @Builder.Default
+    @Column
+    private Authority authority;
 }
