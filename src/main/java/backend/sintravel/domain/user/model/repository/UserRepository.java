573674d1f3271aa85c8ed5ebfd46e7ca7df23712
jihwan2da <88091743+jihwan2da@users.noreply.github.com>
2022-05-31 00:25:02 +0900

    feat: UserRepository에 findByUsername 추가

--- src/main/java/backend/sintravel/domain/user/model/repository/UserRepository.java
+++ src/main/java/backend/sintravel/domain/user/model/repository/UserRepository.java
@@ -3,5 +3,10 @@ package backend.sintravel.domain.user.model.repository;
 import backend.sintravel.domain.user.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
 }
