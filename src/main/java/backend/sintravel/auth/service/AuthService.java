8ab779048d0558a658ca2d09a6ec7e079a7b44f8
jihwan2da <88091743+jihwan2da@users.noreply.github.com>
2022-05-31 00:29:08 +0900

    feat: AuthService 구현

---
+++ src/main/java/backend/sintravel/auth/service/AuthService.java
@@ -0,0 +1,45 @@
+package backend.sintravel.auth.service;
+
+import backend.sintravel.auth.dto.AuthTokenDto;
+import backend.sintravel.auth.exception.CantSignInException;
+import backend.sintravel.auth.jwt.JwtProvider;
+import backend.sintravel.domain.user.exception.UserNotExistException;
+import backend.sintravel.domain.user.model.entity.User;
+import backend.sintravel.domain.user.model.enums.Authority;
+import backend.sintravel.domain.user.model.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final JwtProvider jwtProvider;
+    private final UserRepository userRepository;
+
+    public AuthTokenDto signIn(String username, String password) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UserNotExistException("존재하지 않는 회원입니다"));
+        //passwordEn 추가예정
+        if (!user.getPassword().equals(password)) {
+            throw new CantSignInException("비밀 번호 일치 x");
+        }
+        String token = jwtProvider.createToken(username);
+
+        return new AuthTokenDto(token);
+    }
+
+    //passwordEncoder 추가에정
+    public void signUp(String nickname, String username, String password) {
+
+        User user = new User(
+                null,
+                   username,
+                   password,
+                   nickname,
+                   null,
+                   Authority.USER);
+
+        userRepository.save(user);
+    }
+}
