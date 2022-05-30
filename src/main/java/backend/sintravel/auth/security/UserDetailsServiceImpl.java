185407e077316668a00360179811360d7b9bdfb2
jihwan2da <88091743+jihwan2da@users.noreply.github.com>
2022-05-31 00:27:40 +0900

    feat: UserDetailsServiceImpl 구현

---
+++ src/main/java/backend/sintravel/auth/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package backend.sintravel.auth.security;
+
+import backend.sintravel.domain.user.model.entity.User;
+import backend.sintravel.domain.user.model.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("not find user"));
+
+        return new UserAdaptor(user);
+    }
+}
