e76eb58df7e3014521eed91d8253f965908d7699
jihwan2da <88091743+jihwan2da@users.noreply.github.com>
2022-06-02 05:12:34 +0900

    feat: Board DTO 구현

---
+++ src/main/java/backend/sintravel/domain/board/model/dto/BoardInfo.java
@@ -0,0 +1,17 @@
+package backend.sintravel.domain.board.model.dto;
+
+import backend.sintravel.domain.board.model.entity.Board;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class BoardInfo {
+    private Long boardId;
+    private String title;
+
+    public BoardInfo(Board board) {
+        this.boardId = board.getId();
+        this.title = board.getTitle();
+    }
+}
