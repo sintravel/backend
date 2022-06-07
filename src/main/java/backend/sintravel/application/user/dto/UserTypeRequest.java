7ea06d2d238df25c90d61b0e4d65599266879808
jihwan2da <88091743+jihwan2da@users.noreply.github.com>
2022-06-08 01:58:10 +0900

    feat: UserTypeRequest 구현

---
+++ src/main/java/backend/sintravel/application/user/dto/UserTypeRequest.java
@@ -0,0 +1,10 @@
+package backend.sintravel.application.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class UserTypeRequest {
+    private String type;
+}
