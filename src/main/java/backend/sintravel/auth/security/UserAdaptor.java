3511d9b03775441e6dabb42b04c37f1080d7f08d
jihwan2da <88091743+jihwan2da@users.noreply.github.com>
2022-06-08 01:55:51 +0900

    fix: UserAdaptor @Getter 추가

--- src/main/java/backend/sintravel/auth/security/UserAdaptor.java
+++ src/main/java/backend/sintravel/auth/security/UserAdaptor.java
@@ -1,6 +1,7 @@
 package backend.sintravel.auth.security;
 
 import backend.sintravel.domain.user.model.entity.User;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.ArrayList;
 import java.util.Collection;
 
+@Getter
 public class UserAdaptor implements UserDetails {
 
     private final User user;
