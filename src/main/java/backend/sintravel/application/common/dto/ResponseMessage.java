0fcb9b469183427bcb844368c8ccb33d9195d45e
jihwan2da <88091743+jihwan2da@users.noreply.github.com>
2022-06-08 01:57:50 +0900

    feat: ResponseSuccessMessage 구현

---
+++ src/main/java/backend/sintravel/application/common/dto/ResponseMessage.java
@@ -0,0 +1,10 @@
+package backend.sintravel.application.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ResponseMessage {
+    private String message;
+}
