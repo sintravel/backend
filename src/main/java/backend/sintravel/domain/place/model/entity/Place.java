0ad56b752cdf8194d54ed711021935d32ec7ebaa
jihwan2da <88091743+jihwan2da@users.noreply.github.com>
2022-06-13 08:30:26 +0900

    feat: place entity 구현

--- src/main/java/backend/sintravel/domain/place/model/entity/Place.java
+++ src/main/java/backend/sintravel/domain/place/model/entity/Place.java
@@ -1,16 +1,19 @@
 package backend.sintravel.domain.place.model.entity;
 
-import lombok.Getter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
+@Setter
 @Getter
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@AllArgsConstructor
 public class Place {
 
     @Id
-    private String id;
+    private String place_id;
 
     private String place_name;
 
@@ -38,9 +41,29 @@ public class Place {
 
     private String animal;
 
-    private String keword;
+    private String keyword;
 
     private String evalnum;
 
     private String rate;
+
+    public void createPlace(List<String> placeFields) {
+        this.place_id = placeFields.get(0);
+        this.place_name = placeFields.get(1);
+        this.category_name = placeFields.get(2);
+        this.category_group_code = placeFields.get(3);
+        this.category_group_name= placeFields.get(4);
+        this.phone = placeFields.get(5);
+        this.address_name = placeFields.get(6);
+        this.road_address_name = placeFields.get(7);
+        this.x = placeFields.get(8);
+        this.y = placeFields.get(9);
+        this.place_url = placeFields.get(10);
+        this.distance = placeFields.get(11);
+        this.wheelchair = placeFields.get(12);
+        this.animal = placeFields.get(13);
+        this.keyword = placeFields.get(14);
+        this.evalnum = placeFields.get(15);
+        this.rate = placeFields.get(16);
+    }
 }
