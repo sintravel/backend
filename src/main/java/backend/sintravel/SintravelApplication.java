4f9fa8a17cba0232fe928b60807bc9c75bc3a652
jihwan2da <88091743+jihwan2da@users.noreply.github.com>
2022-06-13 08:43:36 +0900

    feat: @EnableBatchProcessing 어노테이션 추가

--- src/main/java/backend/sintravel/SintravelApplication.java
+++ src/main/java/backend/sintravel/SintravelApplication.java
@@ -1,9 +1,11 @@
 package backend.sintravel;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableBatchProcessing
 @EnableJpaAuditing
 @SpringBootApplication
 public class SintravelApplication {
